cd4a8455ab0d0ff3aab44a5eacb2af90f7e81ae1
EzeG92 <gim24001@byui.edu>
2025-08-04 22:04:57 -0300

    update rest files

--- routes/enrollment.js
+++ routes/enrollment.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const enrollmentController = require("../controllers/enrollmentController");
+const { isAuthenticated } = require('../middleware/authenticate');
 
 
-router.get("/student/:id", enrollmentController.getEnrollmentsByStudent);
-router.get("/course/:id", enrollmentController.getEnrollmentsByCourse);
-router.post("/", enrollmentController.enrollStudent);
-router.delete("/:id", enrollmentController.dropEnrollment);
-router.put("/:id/progress", enrollmentController.updateEnrollmentProgress);
+router.get("/student/:id",
+    enrollmentController.getEnrollmentsByStudent
+);
+
+router.get("/course/:id",
+    enrollmentController.getEnrollmentsByCourse
+);
+
+router.post("/",
+    isAuthenticated,
+    enrollmentController.enrollStudent
+);
+
+router.put("/:id/progress",
+    isAuthenticated,
+    enrollmentController.updateEnrollmentProgress
+);
+
+router.delete("/:id",
+    isAuthenticated,
+    enrollmentController.dropEnrollment
+);
+
 
 module.exports = router;
